bd50d854af26fdc3378ccd6d47ea9499f9efe15c
Paul Dann <pgdann@gmail.com>
2022-06-29 10:03:22 +0100

    Add asdf-direnv and a few misc accumulated tweaks

--- .zprezto-extra/misc/init.zsh
+++ .zprezto-extra/misc/init.zsh
@@ -26,6 +26,10 @@ fpath=(${ASDF_DIR}/completions $fpath)
 autoload -Uz compinit
 compinit
 
+# asdf-direnv
+export DIRENV_LOG_FORMAT=""
+source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
+
 # ENV
 PATH=$PATH:~/Tools
 EDITOR=nvim
@@ -52,11 +56,13 @@ export PULSE_LATENCY_MSEC=100
 alias config="GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME $SHELL"
 
 # Vim
-alias vim=nvim
+alias -g vim=nvim
+alias -g vimdiff=nvimdiff
 alias ecx='edit-compressed-xml'
 
 # Generic Docker
 alias dc="docker-compose"
+alias dcp="docker-compose --profile"
 alias dcr="docker-compose run --rm"
 alias dce="docker-compose exec"
 
@@ -81,9 +87,11 @@ alias wiki="cd ~/Nextcloud/Documents/Wiki/src && vim index.md"
 alias wwiki="cd ~/Nextcloud/Documents/Wiki/src && vim work/index.md"
 alias restart-plasma="kquitapp5 plasmashell && sleep 3 && kstart5 plasmashell"
 alias kill-telepathy="ps -fA | egrep telepathy\\\\/ > >(cat) > >(awk '{ print \$2 }' | xargs -L1 kill)"
-alias pacfiles="locate .pacnew; locate .pacsave; locate .pacorig"
+alias pacfiles="locate .pacnew; locate .pacorig"
 alias oldlibs="sudo lsof +c 0 | grep -w DEL | awk '1 { print \$1 \": \" \$NF }' | sort -u"
-alias music='ncmpcpp'
+alias music="ncmpcpp"
+alias projsplit="tmux split-window -h -l 71%"
+alias tmuxn="tmux new-session"
 
 if (( $+commands[bat] )); then
   alias cat="bat --style=plain"
@@ -93,4 +101,4 @@ fi
 alias sc="sudo systemctl"
 alias scu="systemctl --user"
 alias p="yay"
-alias update="p -Syu"
+alias update="p -Syu && flatpak update"
